93f994ac1df8aeaa0483c2d5a8e8c568ff8b9d00
livia <livia.c.silva6@aluno.senai.br>
2025-04-03 09:19:10 -0300

    vetor

--- main.go
+++ main.go
@@ -9,5 +9,22 @@ func main() {
     fmt.Println(nomes)
 	nomes[3] = "clark kent"
 	fmt.Println(nomes) 
-		
+		// Slice 
+		var score = []int{100, 200, 300, 400}
+		fmt.Println(score)
+		score[1] = 2
+		fmt.Println(score)
+		fmt.Println(score, cap(score))
+		rangeOne := score[1:3]
+		fmt.Println(rangeOne)
+		rangeTwo := score[2:]
+		fmt.Println(rangeTwo)
+		rangethree := score[:3]
+		fmt.Println(rangethree)
+		var superherois = []string{"deadpool", "homemaranha", "motoquerio fantasma"}
+		fmt.Println(superherois)
+		superherois = append(superherois, "ben10", "hulk")
+		fmt.Println(superherois)
+		fmt.Println(superherois, len(superherois), cap(superherois))
+	
 	}
