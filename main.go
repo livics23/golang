ff0c03d31cd9b35d2ef9284c8e877bbc6e0bd627
livia <livia.c.silva6@aluno.senai.br>
2025-04-10 10:25:42 -0300

    idade

--- main.go
+++ main.go
@@ -1,51 +1,15 @@
 package main
-
-import (
-	"fmt"
-	"strings"
-	"sort"
-	
-)
-func main() {
-	greeting := "hello my friends!"
-	fmt.Println(strings.Contains(greeting, "friends"))
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "my"))
-	fmt.Println(strings.Split(greeting, "!"))
-	ages := []int {90, 80, 900}
-	sort.Ints(ages)
-	fmt.Println(ages)
-	index := sort.SearchInts(ages, 80)
-	fmt.Println(index)
-	names := []string{"carol", "maicon", "diegoike"}
-	sort.Strings(names)
-	fmt.Println(names)
-	fmt.Println(sort.SearchStrings(names,"diegoike"))
-	x := 0
-	
-	for x < 5 {
-		fmt.Println(x)
-		x++
-	}
-	
-	for i :=0; i >5; i++{
-		fmt.Println("for 2: ", i)
-	}
-	for i:=0; i <len(names); i++{
-		fmt.Println(names[i])
-	}
-	for index, value := range names{
-		fmt.Println("índice é", index, "e o valor", value,)
-	}
-
-	for index, value := range ages{
-		fmt.Println("o indice é", index, "e o valor", value)
-
-	}
-
-	superheois := []string{"deadpool", }
-	
-	
+import "fmt"
+ 
+ func main() {
+	var idade int
+fmt.Println("digite sua idade")
+fmt.Scan(&idade)
+if idade >=18 || idade <60 {
+	fmt.Println("voce é adulto")
+} else if idade < 18 {
+	fmt.Println("voce é menor de idade")
+} else if idade >= 60 {
+fmt.Println("voce é idoso")
 }
-	
+ }
