9e7ae30f267b189fb01a16f8c8628404814922dd
livia <livia.c.silva6@aluno.senai.br>
2025-04-10 11:04:49 -0300

    saldo

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 import "fmt"
  
  func main() {
-	var idade int
-fmt.Println("digite sua idade")
-fmt.Scan(&idade)
-if idade >=18 || idade <60 {
-	fmt.Println("voce é adulto")
-} else if idade < 18 {
-	fmt.Println("voce é menor de idade")
-} else if idade >= 60 {
-fmt.Println("voce é idoso")
-}
+	var saldo int
+	var digite int 
+	var deposito int
+	var saque int
+	var saquefinal int
+	var depositofinal int
+
+
+fmt.Println("digite seu saldo")
+fmt.Scan(&saldo)
+fmt.Println("digite 1 para depositar e 2 para saque")
+fmt.Scan(&digite)
+
+
+if digite == 1{
+	fmt.Println("coloque o valor do seu deposito")
+	fmt.Scan(&deposito)
+	depositofinal = saldo + deposito
+	fmt.Println("seu saldo final é :", depositofinal)
+} else if digite == 2 {
+	fmt.Println("coloque o valor que deseja retirar")
+	fmt.Scan(&saque)
+	saquefinal = saldo - saque 
+	fmt.Println("seu saldo final é :", saquefinal)
+} 
  }
