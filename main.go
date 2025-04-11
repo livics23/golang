cdc25a06d8f6d09bd39998bc9eee0ace2e946952
livia <livia.c.silva6@aluno.senai.br>
2025-04-11 14:19:41 -0300

    função2

--- main.go
+++ main.go
@@ -1,30 +1,37 @@
 package main
-import "fmt"
- 
- func main() {
-	var saldo int
-	var digite int 
-	var deposito int
-	var saque int
-	var saquefinal int
-	var depositofinal int
+
+import ("fmt")
+
+var opção int
+var saldo int
 
 
-fmt.Println("digite seu saldo")
-fmt.Scan(&saldo)
-fmt.Println("digite 1 para depositar e 2 para saque")
-fmt.Scan(&digite)
-
-
-if digite == 1{
-	fmt.Println("coloque o valor do seu deposito")
-	fmt.Scan(&deposito)
-	depositofinal = saldo + deposito
-	fmt.Println("seu saldo final é :", depositofinal)
-} else if digite == 2 {
-	fmt.Println("coloque o valor que deseja retirar")
-	fmt.Scan(&saque)
-	saquefinal = saldo - saque 
-	fmt.Println("seu saldo final é :", saquefinal)
-} 
+ func deposito() { 
+	 var deposito int 
+	 fmt.Println("digite o valor do deposito")
+	 fmt.Scan(&deposito)
+	 saldo = saldo + deposito
+	fmt.Println("seu saldo atual é:", saldo)
  }
+
+ func saque() {
+	var saque int
+	fmt.Println("digite o valor do saque")
+	fmt.Scan(&saque)
+	saldo = saldo - saque
+	fmt.Println("seu saldo atual é:", saldo)
+ }
+ 
+func main(){
+fmt.Println("digite seu saldo atual")
+fmt.Scan(&saldo)
+ fmt.Println("digite 1 para depositar e 2 para saque")
+ fmt.Scan(&opção)
+
+ if opção == 1 {
+	 deposito()
+ } else if opção == 2 {
+	saque()
+ } 
+ 
+}
