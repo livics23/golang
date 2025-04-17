4fc64c05f041c884cccd51d7e447f8bd8c17a067
livia <livia.c.silva6@aluno.senai.br>
2025-04-17 10:03:51 -0300

    funcaovariav

--- main.go
+++ main.go
@@ -1,29 +1,20 @@
+
 package main
 
 import ("fmt")
-func dividir(dividendo int, divisor int)(int, string){
-	if divisor == 0 {
-		return 0, "erro na divisão"
-	}
-return dividendo / divisor, ""
-}
-func operacaoBasica(a int, b int)(int, int, int){
-	soma:= a +b 
-	multiplicacao:= a * b
-	subtracao:= a - b 
-	return soma, multiplicacao, subtracao 
+func dadosPessoa(idade int) (int, string){
+	var condicao string
+if idade > 18 {
+	condicao = "maior de idade"
 
+} else {
+	condicao = "menor de idade"
+} 
+return idade, condicao
 }
 
 func main(){
-resultado, erro := dividir(10, 0)
-if erro != "sem erro" {
-	fmt.Println(erro)
-} else {
-	fmt.Println("o resultado da divisao é:", resultado)
-} 
-soma, mult, sub:= operacaoBasica(10, 2)
-fmt.Println(soma)
-fmt.Println(mult)
-fmt.Println(sub)
+var idade int 
+ idade, condicao:= dadosPessoa(20)
+fmt.Println(idade, condicao)
 }
