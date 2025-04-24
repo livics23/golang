3fad49dcfc98831c593d5a96314e3c8aa6609184
livia <livia.c.silva6@aluno.senai.br>
2025-04-24 15:26:10 -0300

    pedidoss

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
-
 import "fmt"
-func main() {
-capitais := map[string]string{
-"SP": "São Paulo",
-"RJ": "Rio de Janeiro",
-"ES": "Espirito Santo",
-}
-delete(capitais, "SP")
 
-capitais ["BH"] = "Belo Horizonte"
-for k,v := range capitais {
-fmt.Println("Sigla, Nome", k, v)
-}
+func main(){
+	estoque := map[string]int{
 
+
+	"coxinha" :  10,
+	"pao de queijo" : 15,
+   "refrigerante" : 20,
+}
+	
+	estoque["coxinha"] -=2
+   estoque["pao de queijo"] -=1
+	
+
+	fmt.Println("estoque atual:")
+	for produto , quantidade := range estoque{
+		fmt.Printf("%s, %d \n", produto, quantidade) 
+	}
 }
