d3f32868e2f1bbda2342922a62ad6073c141635e
livia <livia.c.silva6@aluno.senai.br>
2025-04-24 13:55:06 -0300

    anasarnotas

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
-
 package main
 
-import ("fmt")
-func main(){
-	alunoIdade := make(map[string]int)
-	alunoIdade ["livia"] = 16 
-	alunoIdade ["laura"] = 15
-	fmt.Println("idade livia", alunoIdade["livia"])
-notasAluno := map[string]float64{
-	"livia" :1.6,
-     "kelly": 8.5,
-}
-for nome, nota := range notasAluno{
-	fmt.Println("%s tirou a nota %.1f \n", nome, nota,)
-}
-}
+import (
+    "fmt"
+)
+func analisarNotas (nota1, nota2 float64) (float64, string) {
+	 media := (nota1 + nota2) / 2 
+	if media >= 6 {
+		return media, "aprovado"
+	}else{
+	return media, "reprovado"
+	
+	}
+    
+    }
+    func main() {
+    media, resultado := analisarNotas (7.5, 5.5)
+    fmt.Println("Média:", media)
+    fmt.Println("Resultado:", resultado)
+    }
